a484ce56b92b1a869d16a7e02a59b007e298f200
Alverdi Mattia <mattia.alverdi@studenti.unipd.it>
2024-07-23 10:10:01 +0200

    by secr

--- Modbus4QUAX/QUAX_bot.py
+++ Modbus4QUAX/QUAX_bot.py
@@ -1,5 +1,5 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes
+from telegram import Bot, Update
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, Updater
 import logging
 
 logging.basicConfig(
@@ -7,11 +7,23 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+# Sostituisci 'YOUR_BOT_TOKEN' con il token del tuo bot
+TOKEN = '7336207064:AAEBqyI5fLsx6QH_slMXsFRp3o80FKRlbbs'
+
+# Chat ID dove inviare i messaggi
+CHAT_ID = 'YOUR_CHAT_ID'
+
+# Inizializza il bot
+bot = Bot(token=TOKEN)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hello, I'm QUAX_bot! How can I help you?")
     
 async def PressureUpdate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hello, I'm QUAX_bot! How can I help you?")    
+
+# Funzione per inviare un messaggio in chat
+async def send_text_message(text: str) -> None:
+    bot.send_message(chat_id=CHAT_ID, text=text)
 
 def QUAX_bot() -> None:
     application = Application.builder().token("7336207064:AAEBqyI5fLsx6QH_slMXsFRp3o80FKRlbbs").build()
